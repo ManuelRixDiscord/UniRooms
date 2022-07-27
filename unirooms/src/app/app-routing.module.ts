dff005dde6431f6c6f5f65071787b2a309fbb775
ManuelRix <manuelrixworkmail@gmail.com>
2022-07-28 01:55:04 +0200

    Creato il routing system per gestire la navigazione tra le pagine, create le pagine che utilizza la sidebar

--- unirooms/src/app/app-routing.module.ts
+++ unirooms/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatsComponent } from './chats/chats.component';
+import { GroupsComponent } from './groups/groups.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'chats',
+    component:ChatsComponent
+  },
+  {
+    path:'groups',
+    component:GroupsComponent
+  },
+  {
+    path:'settings',
+    component:SettingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- unirooms/src/app/app.module.ts
+++ unirooms/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { ChatsComponent } from './chats/chats.component';
+import { GroupsComponent } from './groups/groups.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
+    HomeComponent,
+    ChatsComponent,
+    GroupsComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
