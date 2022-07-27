6398556542b0481ae5ec6775a812ba5e397b0949
ManuelRix <manuelrixworkmail@gmail.com>
2022-07-27 11:03:34 +0200

    eliminato il frame per semplificare l'app

--- unirooms/src/app/app.module.ts
+++ unirooms/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrameComponent
   ],
   imports: [
     BrowserModule,
