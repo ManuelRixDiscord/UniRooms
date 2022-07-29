98c78f34e3da19d59ec83349a11e2ad876bd1cb2
ManuelRix <manuelrixworkmail@gmail.com>
2022-07-29 02:57:20 +0200

    .

--- unirooms/src/app/chats/chats.component.ts
+++ unirooms/src/app/chats/chats.component.ts
@@ -9,6 +9,9 @@ export class ChatsComponent implements OnInit {
 
   pfpPaperino: string = "../assets/images/paperinopfp.jpg";
   pfpPippo: string = "../assets/images/pippopfp.jpg";
+  callIcon: string = "../assets/images/callingIcon.svg";
+  camIcon: string = "../assets/images/camIcon.svg";
+  optionIcon: string = "../assets/images/optionIcon.svg";
 
   constructor() { }
 
